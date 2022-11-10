ba5c3920566fd0b55cdc58c311e0952512945859
seoruosa <thiago.giachetto@gmail.com>
2022-11-10 12:21:03 -0300

    WIP create a class with the instance data

---
+++ libs/read_instance/mmurp_data.cpp
@@ -0,0 +1,18 @@
+#include "mmurp_data.h"
+#include "mmurp.h"
+
+MMURP_data::MMURP_data(std::string instance_path) : instance_path(instance_path)
+{
+    // TODO need to modify the read_instance to this shouldnt be necessary
+    int int_vehicle_capacity;
+
+    read_instance(MMURP_data::dist_nodes_nodes, MMURP_data::dist_depots_nodes,
+                    MMURP_data::demand, int_vehicle_capacity, MMURP_data::number_vehicles, 
+                    MMURP_data::max_travel_dist, "", instance_path);
+    
+    MMURP_data::vehicle_capacity = float(int_vehicle_capacity);
+}
+
+MMURP_data::~MMURP_data()
+{
+}
---
+++ libs/read_instance/mmurp_data.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+class MMURP_data
+{
+public:
+    MMURP_data(std::string path);
+    ~MMURP_data();
+
+    // path of the instance   
+    std::string instance_path;
+
+    // distance between the clients
+    std::vector<std::vector<float>> dist_nodes_nodes;
+
+    // distance between the clients and depots
+    std::vector<std::vector<float>> dist_depots_nodes;
+
+    // demand of each client
+    std::vector<float> demand;
+
+    // number of vehicles of each depot
+    std::vector<int> number_vehicles;
+
+    // capacity of vehicles
+    float vehicle_capacity;
+
+    // max allowed travelling distance between 2 clients
+    float max_travel_dist;
+};
